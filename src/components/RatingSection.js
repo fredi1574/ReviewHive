e9ed2d7f87fd4a22aa3ef0e414ff170f9092576e
fredi1574 <fredi1574@gmail.com>
2025-02-17 01:20:27 +0200

    Added current rating of the profile

---
+++ src/components/RatingSection.js
@@ -0,0 +1,28 @@
+import { Star } from "lucide-react";
+import Separator from "./ui/Separator";
+
+const RatingSection = () => {
+  const numberOfRates = 100;
+  return (
+    <>
+      <Separator title="Rating Distribution" />
+      <div>
+        {[...Array(5)].map((_, index) => (
+          <div key={index} className="flex items-center">
+            {[...Array(5)].map((_, i) => (
+              <Star
+                key={i}
+                className={`fill-yellow-400 text-yellow-400 ${
+                  i <= 4 - index ? "" : "opacity-20"
+                }`}
+              />
+            ))}
+            <span className="ml-2 text-gray-400"> - {numberOfRates}</span>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default RatingSection;
