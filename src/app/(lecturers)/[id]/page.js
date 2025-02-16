6dc094342a03991848133be3f4c520e8f053673c
fredi1574 <fredi1574@gmail.com>
2025-02-17 01:21:26 +0200

    Detailed page of each profile

---
+++ src/app/(lecturers)/[id]/page.js
@@ -0,0 +1,54 @@
+import AddReviewSection from "@/components/AddReviewSection";
+import Rating from "@/components/Rating";
+import RatingDistribution from "@/components/RatingSection";
+import ReportButton from "@/components/ReportButton";
+import ReviewsSection from "@/components/ReviewsSection";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { getProfileById } from "@/lib/databaseFunctions";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
+
+export default async function Profile({ params }) {
+  const { id } = await params;
+
+  const profile = await getProfileById(id);
+  if (!profile) {
+    return (
+      <div className="text-center text-2xl text-red-500">Profile not found</div>
+    );
+  }
+  const { name, institute, departments, courses, rating } = profile;
+
+  return (
+    <Card className="m-4">
+      <div className="flex justify-between px-4 pt-4">
+        <Link
+          href={"/"}
+          className="flex items-center gap-2 rounded-md px-2 duration-300 hover:bg-sky-200 active:bg-sky-300"
+        >
+          <ArrowLeft className="size-6" />
+          <p className="text-lg">All profiles</p>
+        </Link>
+        <ReportButton />
+      </div>
+      <CardHeader>
+        <CardTitle className="text-3xl">{name}</CardTitle>
+        <CardDescription>{institute}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>{departments}</p>
+        <p>{courses}</p>
+        <Rating />
+        <RatingDistribution />
+        <AddReviewSection />
+        <ReviewsSection />
+      </CardContent>
+    </Card>
+  );
+}
