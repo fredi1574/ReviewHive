8cf391c0eb506110ce451a601b46ec9b4d18a9a0
fredi1574 <fredi1574@gmail.com>
2025-02-18 20:22:12 +0200

    Added a function to get all the reviews on a profile

--- src/lib/databaseFunctions.js
+++ src/lib/databaseFunctions.js
@@ -27,3 +27,13 @@ export async function updateRating(profileId, newRating) {
   const docRef = doc(db, "profiles", profileId);
   await updateDoc(docRef, { rating: newRating });
 }
+
+export async function getReviewsByProfileId(profileId) {
+  const reviewsCollectionRef = collection(db, "profiles", profileId, "reviews");
+  const querySnapshot = await getDocs(reviewsCollectionRef);
+  const reviews = querySnapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+  return reviews;
+}
