38182854486b9c77c7a15802597fd18bc1945948
fredi1574 <fredi1574@gmail.com>
2025-02-21 19:49:43 +0200

    Show a real count for each category

--- src/components/RatingDistribution.js
+++ src/components/RatingDistribution.js
@@ -1,28 +1,23 @@
 import { Star } from "lucide-react";
-import Separator from "./ui/Separator";
 
-const RatingSection = () => {
-  const numberOfRates = 100;
+const RatingDistribution = ({ ratingCounts }) => {
   return (
-    <>
-      <Separator title="Rating Distribution" />
-      <div className="my-4 flex flex-col gap-2">
-        {[...Array(5)].map((_, index) => (
-          <div key={index} className="flex items-center">
-            {[...Array(5)].map((_, i) => (
-              <Star
-                key={i}
-                className={`fill-yellow-400 text-yellow-400 ${
-                  i <= 4 - index ? "" : "opacity-20"
-                }`}
-              />
-            ))}
-            <span className="ml-2 text-gray-400"> - {numberOfRates}</span>
-          </div>
-        ))}
-      </div>
-    </>
+    <div className="my-4 flex flex-col gap-2">
+      {[5, 4, 3, 2, 1].map((star) => (
+        <div key={star} className="flex items-center">
+          {[...Array(5)].map((_, i) => (
+            <Star
+              key={i}
+              className={`fill-yellow-400 text-yellow-400 ${
+                i < star ? "" : "opacity-20"
+              }`}
+            />
+          ))}
+          <span className="ml-2 text-gray-400">{` ${ratingCounts[star] !== undefined ? " - " + ratingCounts[star] : ""} `}</span>
+        </div>
+      ))}
+    </div>
   );
 };
 
-export default RatingSection;
+export default RatingDistribution;
