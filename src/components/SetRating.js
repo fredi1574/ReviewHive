2edc527b64dce3e95cb146c3cb1193d844cd47d9
fredi1574 <fredi1574@gmail.com>
2025-02-21 19:53:04 +0200

    Changed the style of the component

--- src/components/SetRating.js
+++ src/components/SetRating.js
@@ -1,30 +1,23 @@
 "use client";
-import { updateRating } from "@/lib/databaseFunctions";
 import { Star } from "lucide-react";
-import { useParams } from "next/navigation";
 import { useState } from "react";
 
 const SetRating = () => {
-  const { id } = useParams();
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
-
-  const changeRating = async (newRating) => {
-    setRating(newRating);
-    await updateRating(id, newRating);
-  };
 
   return (
     <div className="flex">
       <p className="mr-2">Your Rating:</p>
-      {rating != 0 && <span className="mr-2">{rating}</span>}
       {[...Array(5)].map((_, index) => {
         const starValue = index + 1;
         return (
           <button
             key={starValue}
             type="button"
-            onClick={() => changeRating(starValue)}
+            onClick={() => {
+              setRating(starValue);
+            }}
             onMouseEnter={() => setHover(starValue)}
             onMouseLeave={() => setHover(rating)}
           >
@@ -32,13 +25,14 @@ const SetRating = () => {
               className={
                 starValue <= (hover || rating)
                   ? "fill-yellow-400 text-yellow-400"
-                  : "text-gray-400"
+                  : "text-gray-300"
               }
             />
           </button>
         );
       })}
       <input type="hidden" name="rating" value={rating} />
+      {rating != 0 && <span className="ml-2">({rating})</span>}
     </div>
   );
 };
