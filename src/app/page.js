886c752f34f1225d0a34b2475f27f30ec1d8e8bc
fredi1574 <fredi1574@gmail.com>
2025-02-21 19:44:58 +0200

    Put the header at the top

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import CardsContainer from "@/components/CardsContainer";
+import Header from "@/components/Header";
 
-export default function Home() {
+export default function Home({ searchParams }) {
   return (
     <main>
-      <CardsContainer />
+      <Header />
+      <CardsContainer searchParams={searchParams} />
     </main>
   );
 }
