047a7ffaf549214e01a5176bd0bc4d8fcb7de889
fredi1574 <fredi1574@gmail.com>
2025-02-21 19:50:11 +0200

    Changed the style

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,28 +1,38 @@
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
 import ReportButton from "./ReportButton";
 import ReviewRating from "./ReviewRating";
-import Separator from "./ui/Separator";
 
-export default async function Review({ review }) {
+export default function Review({ review }) {
   const { author, body, rating, timestamp } = review;
 
   return (
-    <div>
-      <div className="my-4 flex flex-col">
-        <div className="flex justify-between">
-          <p className="max-w-[95%]">{body}</p>
-          <ReportButton />
+    <Card className="my-4">
+      <CardHeader className="flex flex-row items-center justify-between">
+        <div className="flex items-center space-x-2">
+          <Avatar>
+            <AvatarFallback>{author[0].toUpperCase()}</AvatarFallback>
+          </Avatar>
+          <div>
+            <p className="text-sm font-medium">{author}</p>
+            <p className="text-xs text-muted-foreground">
+              {new Date(timestamp.seconds * 1000).toLocaleDateString()}
+            </p>
+          </div>
         </div>
-        <div className="flex gap-2">
-          <ReviewRating rating={rating} />
-          <p className="text-sm text-gray-400">|</p>
-          <p className="text-sm italic text-gray-400">{author}</p>
-          <p className="text-sm text-gray-400">|</p>
-          <p className="text-sm text-gray-400">
-            {new Date(timestamp.seconds * 1000).toLocaleDateString()}
-          </p>
-        </div>
-      </div>
-      <Separator />
-    </div>
+        <ReportButton />
+      </CardHeader>
+      <CardContent>
+        <p className="text-sm">{body}</p>
+      </CardContent>
+      <CardFooter>
+        <ReviewRating rating={rating} />
+      </CardFooter>
+    </Card>
   );
 }
