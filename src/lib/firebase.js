fda8771822ee2597bad9dcf160a575452128ff6b
Fredi1574 <Fredi1574@gmail.com>
2025-03-13 15:57:48 +0200

    Removed authentication in the site

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,5 +1,4 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -13,7 +12,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth();
 const db = getFirestore(app);
 
-export { auth, db, GoogleAuthProvider };
+export { db };
